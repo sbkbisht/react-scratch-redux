3aef2e87c2f7ea053c2b38ad0d9923afb31b4164
sbkbisht <sumanbishtbk@gmail.com>
2022-07-13 21:06:11 +0530

    actin created to use thunk

--- src/todos/actions.js
+++ src/todos/actions.js
@@ -17,3 +17,25 @@ export const markTodoAsCompleted = (text) => ({
   type: MARK_TODO_AS_COMPLETED,
   payload: { text },
 });
+
+export const LOAD_TODOS_IN_PROGRESS = "LOAD_TODOS_IN_PROGRESS"; // action type
+
+// action creator
+export const loadTodosInProgress = () => ({
+  type: LOAD_TODOS_IN_PROGRESS,
+});
+
+export const LOAD_TODOS_IN_SUCCESS = "LOAD_TODOS_IN_SUCCESS"; // action type
+
+// action creator
+export const loadTodosInSuccess = (todos) => ({
+  type: LOAD_TODOS_IN_SUCCESS,
+  payload: { todos },
+});
+
+export const LOAD_TODOS_IN_FAILURE = "LOAD_TODOS_IN_FAILURE"; // action type
+
+// action creator
+export const loadTodosFailure = () => ({
+  type: LOAD_TODOS_IN_FAILURE,
+});
--- src/todos/thunk.js
+++ src/todos/thunk.js
@@ -1,8 +1,28 @@
 // Redux thunk is simply a function that returns another function which contains the actually logic and performance that we want to perform when triggered
 
-// it return another function from main function
-export const displayAlert = () => () => {
-  alert("Hello");
-};
+import {
+  loadTodosInSuccess,
+  loadTodosInProgress,
+  loadTodosFailure,
+} from "./actions";
 
 // how to dispatch thunk, without thunk in redux when we dispatch action then it take type(string, action name) and payload (object, or data to attached)
+
+export const loadTodos = () => async (dispatch, getState) => {
+  try {
+    dispatch(loadTodosInProgress());
+    const response = await fetch("http://localhost:8080/todos");
+    const todos = await response.json();
+
+    dispatch(loadTodosInSuccess(todos));
+  } catch (e) {
+    dispatch(loadTodosFailure(e));
+    dispatch(displayAlert(e));
+  }
+};
+
+// it return another function from main function
+export const displayAlert = (text) => () => {
+  //   alert(`you clicked on: ${text}`);
+  alert(text);
+};
