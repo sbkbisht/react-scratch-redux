9c76d576b35071a0786e11beabd9bc8a3aaddc04
sbkbisht <sumanbishtbk@gmail.com>
2022-07-13 22:53:17 +0530

    add selectors

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,60 +1,50 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
-import NewTodoForm from "./NewTodoForm";
-import TodoListItem from "./TodoListItem";
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import NewTodoForm from './NewTodoForm';
+import TodoListItem from './TodoListItem';
 import {
-  loadTodos,
-  removeTodoRequest,
-  markTodoAsCompletedRequest,
-} from "./thunk";
-// import { markTodoAsCompleted } from "./actions";
-import { displayAlert } from "./thunk";
-import "./TodoList.css";
-import { isLoading } from "./reducers";
+    getTodos,
+    getTodosLoading,
+    getCompletedTodos,
+    getIncompleteTodos,
+} from './selectors';
+import { loadTodos, removeTodoRequest, markTodoAsCompletedRequest } from './thunks';
+import './TodoList.css';
 
-const TodoList = ({
-  todos = [],
-  onRemovePressed,
-  onCompletedPressed,
-  onDisplayAlertClicked,
-  isLoading,
-  startLoadingTodos,
-}) => {
-  useEffect(() => {
-    startLoadingTodos();
-  }, []);
-  const loadingMessage = <div>Loading todos...</div>;
-  const content = (
-    <div className="list-wrapper">
-      <NewTodoForm />
-      {todos.map((todo) => (
-        <TodoListItem
-          todo={todo}
-          onRemovePressed={onRemovePressed}
-          onCompletedPressed={onCompletedPressed}
-          // simple example of use thunk middleware
-          // onCompletedPressed={onDisplayAlertClicked}
-        />
-      ))}
-    </div>
-  );
-  return isLoading ? loadingMessage : content;
+const TodoList = ({ completedTodos, incompleteTodos, onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos }) => {
+    useEffect(() => {
+        startLoadingTodos();
+    }, []);
+    
+    const loadingMessage = <div>Loading todos...</div>;
+    const content = (
+        <div className="list-wrapper">
+            <NewTodoForm />
+            <h3>Incomplete:</h3>
+            {incompleteTodos.map(todo => <TodoListItem
+                todo={todo}
+                onRemovePressed={onRemovePressed}
+                onCompletedPressed={onCompletedPressed}/>)}
+            <h3>Completed:</h3>
+            {completedTodos.map(todo => <TodoListItem
+                todo={todo}
+                onRemovePressed={onRemovePressed}
+                onCompletedPressed={onCompletedPressed}/>)}
+        </div>
+    );
+    return isLoading ? loadingMessage : content;
 };
 
-const mapStateToProps = (state) => ({
-  isLoading: state.isLoading,
-  todos: state.todos,
+const mapStateToProps = state => ({
+    isLoading: getTodosLoading(state),
+    completedTodos: getCompletedTodos(state),
+    incompleteTodos: getIncompleteTodos(state),
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  startLoadingTodos: () => dispatch(loadTodos()),
-  //   onRemovePressed: (text) => dispatch(removeTodo(text)),
-  onRemovePressed: (id) => dispatch(removeTodoRequest(id)),
-  onCompletedPressed: (id) => dispatch(markTodoAsCompletedRequest(id)),
-  // using thunk
-  //this is going to be a function, doesn't take any argument as argument not passed inside thunk.js displayAlert()
-  // you can pass argument if you want
-  onDisplayAlertClicked: () => dispatch(displayAlert()),
+const mapDispatchToProps = dispatch => ({
+    startLoadingTodos: () => dispatch(loadTodos()),
+    onRemovePressed: id => dispatch(removeTodoRequest(id)),
+    onCompletedPressed: id => dispatch(markTodoAsCompletedRequest(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
